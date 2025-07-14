492434ee28343cb7ddbb892e22c5df27358f084b
Aryan Jain Chympa <aryanjjchympa@gmail.com>
2025-07-15 01:46:32 +0530

    new changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/aryanprofile.jpg";
+import profilePic from "../assets/aryanchympa.jpg";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
